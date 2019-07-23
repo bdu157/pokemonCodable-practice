934ee06e4705dc187cc880572eb423791c1fa50f
Dongwoo Pae <dongwoopae@gmail.com>
2019-07-22 23:59:05 -0700

    first commit

---
+++ pokemonCodable-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokemonCodable-practice
+//
+//  Created by Dongwoo Pae on 7/22/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
